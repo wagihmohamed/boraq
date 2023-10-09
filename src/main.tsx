b9fd8d7edef1e773e34e17b3a398fb4afab01048
wagihmohamed <wagihm969@gmail.com>
2023-10-09 23:21:40 +0300

    added mantine UI and handled RTL

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import '@mantine/core/styles.css';
+import { MantineProvider, DirectionProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DirectionProvider>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </DirectionProvider>
+  </React.StrictMode>
+);
