5206d68a221a570fc95d39dfb92818d0572a7c11
wagihmohamed <wagihm969@gmail.com>
2023-10-09 23:50:40 +0300

    added react query

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from './App.tsx';
 import './index.css';
 import '@mantine/core/styles.css';
 import { MantineProvider, DirectionProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <DirectionProvider>
       <MantineProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </MantineProvider>
     </DirectionProvider>
   </React.StrictMode>
