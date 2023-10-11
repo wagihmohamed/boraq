cb00ebc48e2bd3f28463b045254f84cf27b613f2
wagihmohamed <wagihm969@gmail.com>
2023-10-11 14:14:15 +0300

    handled emplyee table with fake data

---
+++ src/screens/Employees/columns.tsx
@@ -0,0 +1,57 @@
+import { Employee } from '@/models';
+import { ColumnDef } from '@tanstack/react-table';
+import { MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export const columns: ColumnDef<Employee>[] = [
+  {
+    accessorKey: 'id',
+    header: '#',
+  },
+  {
+    accessorKey: 'name',
+    header: 'الاسم',
+  },
+  {
+    accessorKey: 'role',
+    header: 'الوظيفة',
+  },
+  {
+    accessorKey: 'branch',
+    header: 'الفرع',
+  },
+  {
+    accessorKey: 'location',
+    header: 'المخزن',
+  },
+  {
+    accessorKey: 'phone',
+    header: 'رقم الهاتف',
+  },
+  {
+    id: 'actions',
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center">
+            <DropdownMenuItem>عرض</DropdownMenuItem>
+            <DropdownMenuItem className="bg-destructive my-3">
+              مسح
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
