a4ca3af3069ea16f684c1d3bc08b72f7b18cfa79
wagihmohamed <wagihm969@gmail.com>
2023-10-17 00:22:16 +0300

    handled edit repository service

--- src/screens/EditBranch/schema.ts
+++ src/screens/EditBranch/schema.ts
@@ -1,11 +1,6 @@
-import { isValidIraqiPhoneNumber } from '@/lib/testIraqiPhoneNumber';
 import { z } from 'zod';
 
 export const editBranchSchema = z.object({
   name: z.string().min(3, { message: 'يجب ان يكون اكثر من 3 حروف' }),
-  email: z.string().email({ message: 'يجب ان يكون بريد الكتروني' }),
-  phone: z.string().refine(isValidIraqiPhoneNumber, {
-    message: 'رقم الهاتف يجب ان يكون رقم عراقي',
-  }),
-  location: z.string().min(1, { message: 'الرجاء اختيار الموقع' }),
+  branch: z.string().min(1, { message: 'الرجاء اختيار الموقع' }),
 });
