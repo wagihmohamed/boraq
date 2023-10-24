6e67b7aeac243c3c007ab77e4f49268b133d0f94
wagihmohamed <wagihm969@gmail.com>
2023-10-24 19:41:27 +0300

    handled stores list service

---
+++ src/screens/Stores/columns.tsx
@@ -0,0 +1,71 @@
+import { ColumnDef } from '@tanstack/react-table';
+import { MoreHorizontal } from 'lucide-react';
+import { Button, buttonVariants } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Link } from 'react-router-dom';
+import { Store } from '@/services/getStores';
+
+export const columns: ColumnDef<Store>[] = [
+  {
+    accessorKey: 'name',
+    header: 'الاسم',
+  },
+  {
+    accessorKey: 'client.name',
+    header: 'العميل',
+    cell: ({ row }) => {
+      const { client } = row.original;
+      return (
+        <Link
+          to={`/clients/${client.id}/show`}
+          className={buttonVariants({
+            variant: 'link',
+            className: 'text-primary-foreground underline',
+          })}
+        >
+          {client.name}
+        </Link>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const location = row.original;
+      return (
+        <DropdownMenu dir="rtl">
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center">
+            <Link
+              className={buttonVariants({
+                variant: 'ghost',
+                className: 'w-full',
+              })}
+              to={`/stores/${location.id}/show`}
+            >
+              عرض
+            </Link>
+            <Link
+              className={buttonVariants({
+                variant: 'ghost',
+                className: 'w-full',
+              })}
+              to={`/stores/${location.id}/edit`}
+            >
+              تعديل
+            </Link>
+            {/* <DeleteLocation id={location.id} /> */}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
