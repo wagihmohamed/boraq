9711caf0ebc642de301b82740090c63e18763ff6
wagihmohamed <wagihm969@gmail.com>
2023-11-18 21:39:05 +0200

    added error toasts for creating reciepts and reports

--- src/hooks/useCreateReport.ts
+++ src/hooks/useCreateReport.ts
@@ -3,10 +3,16 @@ import {
   CreateReportPayload,
   createReportService,
 } from '@/services/createReport';
+import { AxiosError } from 'axios';
+import { APIError } from '@/models';
+import toast from 'react-hot-toast';
 
 export const useCreateReport = ({ onSuccess = () => {} }) => {
   return useMutation({
     mutationFn: (data: CreateReportPayload) => createReportService(data),
     onSuccess,
+    onError: (error: AxiosError<APIError>) => {
+      toast.error(error.response?.data.message || 'حدث خطأ ما');
+    },
   });
 };
--- src/hooks/useOrderReceipt.ts
+++ src/hooks/useOrderReceipt.ts
@@ -1,8 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 import { getOrderReceipt } from '@/services/getOrderReceipt';
+import { AxiosError } from 'axios';
+import { APIError } from '@/models';
+import toast from 'react-hot-toast';
 
 export const useOrderReceipt = (name: string) => {
   return useMutation({
     mutationFn: (id: string[]) => getOrderReceipt(id, name),
+    onError: (error: AxiosError<APIError>) => {
+      toast.error(error.response?.data.message || 'حدث خطأ ما');
+    },
   });
 };
