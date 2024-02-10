b6e239c18f6a8a51fc5d27e5ba822597b39d97aa
Ali Kehel <ali.saber.27.2@gmail.com>
2024-02-10 10:08:13 +0200

    added delivery agent reports to the reports screen

---
+++ src/screens/Reports/components/TabsViews/DeliveryAgentReportsView/index.tsx
@@ -0,0 +1,34 @@
+import { DataTable } from '@/screens/Employees/data-table';
+import { useState } from 'react';
+import { ReportsFilters } from '@/services/getReports';
+import { useReports } from '@/hooks/useReports';
+import { LoadingOverlay } from '@mantine/core';
+import { ReportsFilter } from '../../ReportsFilter';
+import { columns } from './columns';
+
+export const DeliveryAgentReportsView = () => {
+  const [filters, setFilters] = useState<ReportsFilters>({
+    page: 1,
+    size: 10,
+    type: 'DELIVERY_AGENT',
+  });
+  const { data: reports, isInitialLoading } = useReports(filters);
+
+  return (
+    <>
+      <ReportsFilter filters={filters} setFilters={setFilters} />
+      <div className="relative mt-12">
+        <LoadingOverlay visible={isInitialLoading} />
+        <DataTable
+          data={reports?.data?.reports || []}
+          columns={columns}
+          filters={{
+            ...filters,
+            pagesCount: reports?.pagesCount || 0,
+          }}
+          setFilters={setFilters}
+        />
+      </div>
+    </>
+  );
+};
