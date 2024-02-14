ef8b76b83863f58d91314480fd8a2ae6d29131bd
wagihmohamed <wagihm969@gmail.com>
2024-02-14 22:14:15 +0200

    handled grid layout again

--- src/screens/Reports/components/ReportsStatistics.tsx
+++ src/screens/Reports/components/ReportsStatistics.tsx
@@ -19,7 +19,7 @@ export const ReportsStatistics = ({
 }: ReportsStatisticsProps) => {
   return (
     <Grid grow>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="العدد الكلي للطلبات"
           value={
@@ -28,55 +28,55 @@ export const ReportsStatistics = ({
           }
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="عدد طلبات بغداد"
           value={reportsMetaData?.baghdadOrdersCount || 0}
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="عدد طلبات المحافظات"
           value={reportsMetaData?.governoratesOrdersCount || 0}
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="عدد الكشوفات"
           value={reportsMetaData?.reportsCount || 0}
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="التكلفة الكلية"
           value={reportsMetaData?.totalCost || 0}
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="المبلغ المدفوع"
           value={reportsMetaData?.paidAmount || 0}
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="تكلفة التوصيل"
           value={reportsMetaData?.deliveryCost || 0}
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="صافي العميل"
           value={reportsMetaData?.clientNet || 0}
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="صافي الشركة"
           value={reportsMetaData?.companyNet || 0}
         />
       </Grid.Col>
-      <Grid.Col span={{ xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
+      <Grid.Col span={{ base: 6, xs: 3, sm: 3, md: 2, lg: 2, xl: 1 }}>
         <StatisticsItem
           title="صافي المندوب"
           value={reportsMetaData?.deliveryAgentNet || 0}
