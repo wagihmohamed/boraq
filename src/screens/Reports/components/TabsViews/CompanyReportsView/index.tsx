f06be8c63a9499ca0ca15e98509622fcc85dc813
wagihmohamed <wagihm969@gmail.com>
2024-02-15 19:20:09 +0200

    handled company tap in reports

--- src/screens/Reports/components/TabsViews/CompanyReportsView/index.tsx
+++ src/screens/Reports/components/TabsViews/CompanyReportsView/index.tsx
@@ -3,11 +3,21 @@ import { useState } from 'react';
 import { columns } from './columns';
 import { ReportsFilters } from '@/services/getReports';
 import { useReports } from '@/hooks/useReports';
-import { LoadingOverlay } from '@mantine/core';
+import { Divider, LoadingOverlay } from '@mantine/core';
 import { ReportsFilter } from '../../ReportsFilter';
 import { ReportsStatistics } from '../../ReportsStatistics';
+import { OrdersFilter, OrdersMetaData } from '@/services/getOrders';
+import { ordersFilterInitialState } from '@/screens/Orders';
+import { useOrders } from '@/hooks/useOrders';
+import { CompanyOrdersFilter } from './CompanyOrders';
+import { OrdersTable } from '@/screens/Orders/components/OrdersTable';
+import { columns as ordersColumns } from '../../../../Orders/columns';
+import { CompanyOrdersStatistics } from './CompanyOrdersStatistics';
 
 export const CompanyReportsView = () => {
+  const [ordersFilter, setOrdersFilter] = useState<OrdersFilter>(
+    ordersFilterInitialState
+  );
   const [filters, setFilters] = useState<ReportsFilters>({
     page: 1,
     size: 10,
@@ -15,8 +25,39 @@ export const CompanyReportsView = () => {
   });
   const { data: reports, isInitialLoading } = useReports(filters);
 
+  const {
+    data: orders = {
+      data: {
+        orders: [],
+        ordersMetaData: {} as OrdersMetaData,
+      },
+      pagesCount: 0,
+    },
+    isInitialLoading: isOrdersInitialLoading,
+  } = useOrders(ordersFilter);
+
   return (
     <>
+      <CompanyOrdersFilter
+        ordersFilters={ordersFilter}
+        setOrdersFilters={setOrdersFilter}
+      />
+      <div className="relative mt-12 mb-12">
+        <p className="text-center -mb-5 md:text-3xl text-2xl">الطلبات</p>
+        <LoadingOverlay visible={isOrdersInitialLoading} />
+        <OrdersTable
+          columns={ordersColumns}
+          data={orders.data.orders}
+          setFilters={setOrdersFilter}
+          filters={{
+            ...ordersFilter,
+            pagesCount: orders.pagesCount,
+          }}
+        />
+        <CompanyOrdersStatistics orders={orders.data.orders} />
+      </div>
+      <Divider my="md" size="md" color="red" />
+      <Divider my="md" size="md" color="red" />
       <ReportsFilter filters={filters} setFilters={setFilters} />
       <ReportsStatistics reportsMetaData={reports?.data?.reportsMetaData} />
       <div className="relative mt-12">
