06cb8d73bc9dcf0a496f53c36016828c46fc5f3e
wagihmohamed <wagihm969@gmail.com>
2024-02-17 21:50:38 +0200

    handled reports governorate tap filters to include the orders

--- src/screens/Reports/components/TabsViews/GovernorateReportsView/GovernorateOrders.tsx
+++ src/screens/Reports/components/TabsViews/GovernorateReportsView/GovernorateOrders.tsx
@@ -5,19 +5,55 @@ import { DateTimePicker } from '@mantine/dates';
 import 'dayjs/locale/ar';
 import { reportOrderStatuses } from '@/lib/reportOrderStatuses';
 import { governorateArray } from '@/lib/governorateArabicNames ';
+import { ReportsFilters } from '@/services/getReports';
+import { getSelectOptions } from '@/lib/getSelectOptions';
+import { reportStatusArray } from '@/lib/reportStatusArabicNames';
+import { useEmployees } from '@/hooks/useEmployees';
 
 interface GovernorateOrdersProps {
   governorateFilter: OrdersFilter;
   setGovernorateFilter: React.Dispatch<React.SetStateAction<OrdersFilter>>;
+  reportsFilters: ReportsFilters;
+  setReportsFilters: React.Dispatch<React.SetStateAction<ReportsFilters>>;
 }
 
 export const GovernorateOrdersFilters = ({
   governorateFilter,
   setGovernorateFilter,
+  reportsFilters,
+  setReportsFilters,
 }: GovernorateOrdersProps) => {
-  const handleResetRangeDate = () => {
+  const {
+    data: reportCreatedBy = {
+      data: [],
+    },
+  } = useEmployees({
+    size: 1000,
+    minified: true,
+    roles: [
+      'ACCOUNTANT',
+      'ACCOUNT_MANAGER',
+      'BRANCH_MANAGER',
+      'COMPANY_MANAGER',
+      'DATA_ENTRY',
+      'EMERGENCY_EMPLOYEE',
+      'INQUIRY_EMPLOYEE',
+      'RECEIVING_AGENT',
+      'REPOSITORIY_EMPLOYEE',
+    ],
+  });
+
+  const handleResetOrdersRangeDate = () => {
     setGovernorateFilter({
       ...governorateFilter,
+      start_date: null,
+      end_date: null,
+    });
+  };
+
+  const handleResetReportsRangeDate = () => {
+    setReportsFilters({
+      ...reportsFilters,
       start_date: null,
       end_date: null,
     });
@@ -37,16 +73,54 @@ export const GovernorateOrdersFilters = ({
                 ...governorateFilter,
                 governorate: e || '',
               });
+              setReportsFilters({
+                ...reportsFilters,
+                store_id: e || '',
+              });
             }}
             placeholder="اختر المحافظة"
             limit={100}
             data={governorateArray}
           />
         </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 4, lg: 4, sm: 12, xs: 12 }}>
+          <Select
+            value={reportsFilters.created_by_id}
+            allowDeselect
+            label="ساحب الكشف"
+            searchable
+            clearable
+            onChange={(e) => {
+              setReportsFilters({
+                ...reportsFilters,
+                created_by_id: e || '',
+              });
+            }}
+            placeholder="اختر ساحب الكشف"
+            data={getSelectOptions(reportCreatedBy.data)}
+          />
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 4, lg: 4, sm: 12, xs: 12 }}>
+          <Select
+            value={reportsFilters.status}
+            allowDeselect
+            label="حالة الكشف"
+            searchable
+            clearable
+            onChange={(e) => {
+              setReportsFilters({
+                ...reportsFilters,
+                status: e || '',
+              });
+            }}
+            placeholder="اختر الحالة"
+            data={reportStatusArray}
+          />
+        </Grid.Col>
         <Grid.Col span={{ base: 12, md: 6, lg: 4, sm: 12, xs: 12 }}>
           <MultiSelect
             value={governorateFilter.statuses}
-            label="الحالة"
+            label="حالة الطلب"
             searchable
             clearable
             onChange={(e) => {
@@ -55,15 +129,16 @@ export const GovernorateOrdersFilters = ({
                 statuses: e || '',
               });
             }}
-            placeholder="اختر الحالة"
+            placeholder="حالة الطلب"
             data={reportOrderStatuses}
           />
         </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 12, lg: 12, sm: 12, xs: 12 }}>
+        <Grid.Col span={{ base: 12, md: 12, lg: 6, sm: 12, xs: 12 }}>
           <div className="flex items-center gap-4 flex-wrap">
             <DateTimePicker
+              className="w-60"
               valueFormat="DD MMM YYYY hh:mm A"
-              label="بداية تاريخ الكشف"
+              label="بداية تاريخ الطلب"
               value={
                 governorateFilter.start_date
                   ? new Date(governorateFilter.start_date)
@@ -81,8 +156,9 @@ export const GovernorateOrdersFilters = ({
               }}
             />
             <DateTimePicker
+              className="w-60"
               valueFormat="DD MMM YYYY hh:mm A"
-              label="نهاية تاريخ الكشف"
+              label="نهاية تاريخ الطلب"
               placeholder="اختر تاريخ النهاية"
               value={
                 governorateFilter.end_date
@@ -101,7 +177,60 @@ export const GovernorateOrdersFilters = ({
             />
             {governorateFilter.end_date && governorateFilter.start_date && (
               <Button
-                onClick={handleResetRangeDate}
+                onClick={handleResetOrdersRangeDate}
+                className="mt-6"
+                variant="outline"
+              >
+                الحذف
+              </Button>
+            )}
+          </div>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 12, lg: 6, sm: 12, xs: 12 }}>
+          <div className="flex items-center gap-4 flex-wrap">
+            <DateTimePicker
+              className="w-60"
+              valueFormat="DD MMM YYYY hh:mm A"
+              label="بداية تاريخ الكشف"
+              value={
+                reportsFilters.start_date
+                  ? new Date(reportsFilters.start_date)
+                  : null
+              }
+              placeholder="اختر تاريخ البداية"
+              locale="ar"
+              clearable
+              onChange={(date) => {
+                const formattedDate = convertDateFormat(date);
+                setReportsFilters({
+                  ...reportsFilters,
+                  start_date: formattedDate,
+                });
+              }}
+            />
+            <DateTimePicker
+              className="w-60"
+              valueFormat="DD MMM YYYY hh:mm A"
+              label="نهاية تاريخ الكشف"
+              placeholder="اختر تاريخ النهاية"
+              value={
+                reportsFilters.end_date
+                  ? new Date(reportsFilters.end_date)
+                  : null
+              }
+              locale="ar"
+              clearable
+              onChange={(date) => {
+                const formattedDate = convertDateFormat(date);
+                setReportsFilters({
+                  ...reportsFilters,
+                  end_date: formattedDate,
+                });
+              }}
+            />
+            {reportsFilters.end_date && reportsFilters.start_date && (
+              <Button
+                onClick={handleResetReportsRangeDate}
                 className="mt-6"
                 variant="outline"
               >
