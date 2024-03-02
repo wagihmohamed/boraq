cec308da23dfacb6a5a6251b4aa0852c0846a1c9
wagihmohamed <wagihm969@gmail.com>
2024-03-02 03:18:52 +0200

    handled create delivery agent manifest

--- src/services/createOrdersDocumentation.ts
+++ src/services/createOrdersDocumentation.ts
@@ -4,6 +4,7 @@ import { AxiosError, AxiosResponse } from 'axios';
 import FileSaver from 'file-saver';
 import { OrdersFilter } from './getOrders';
 import { getReportParam } from '@/lib/getReportParam';
+import { ManifestFilters } from './getDeliveryAgentManifest';
 
 type CreateAllOrdersDocumentationPDFPayload = {
   ordersIDs: number[] | '*';
@@ -14,12 +15,16 @@ type CreateAllOrdersDocumentationPDFPayload = {
 export type CreateOrdersDocumentationPDFByIDsPayload = {
   ordersIDs: '*';
   type: 'DELIVERY_AGENT_MANIFEST';
-  params: OrdersFilter;
+  params: CreateDeliveryAgentManifestPDFFilters;
 };
 
 export type CreateOrdersDocumentationReportPDFPayload =
   | CreateAllOrdersDocumentationPDFPayload
   | CreateOrdersDocumentationPDFByIDsPayload;
+
+export type CreateDeliveryAgentManifestPDFFilters = {
+  delivery_agent_id: number;
+} & ManifestFilters;
 
 export const createOrdersDocumentationService = async (
   data: CreateOrdersDocumentationReportPDFPayload
@@ -28,42 +33,57 @@ export const createOrdersDocumentationService = async (
     const response = await api.post<
       CreateOrdersDocumentationReportPDFPayload,
       AxiosResponse<never>
-    >(createOrdersDocumentationendpoint, data, {
-      responseType: 'arraybuffer',
-      params: {
-        page: data.params.page,
-        size: data.params.size,
-        search: data.params.search || undefined,
-        sort: data.params.sort || undefined,
-        start_date: data.params.start_date || undefined,
-        end_date: data.params.end_date || undefined,
-        delivery_date: data.params.delivery_date || undefined,
-        governorate: data.params.governorate || undefined,
-        status: data.params.status || undefined,
-        delivery_type: data.params.delivery_type || undefined,
-        delivery_agent_id: data.params.delivery_agent_id || undefined,
-        client_id: data.params.client_id || undefined,
-        store_id: data.params.store_id || undefined,
-        product_id: data.params.product_id || undefined,
-        location_id: data.params.location_id || undefined,
-        receipt_number: data.params.receipt_number || undefined,
-        recipient_name: data.params.recipient_name || undefined,
-        recipient_phone: data.params.recipient_phone || undefined,
-        recipient_address: data.params.recipient_address || undefined,
-        deleted: data.params.deleted,
-        statuses: data.params.statuses?.join(',') || undefined,
-        client_report: getReportParam(data.params.client_report),
-        repository_report: getReportParam(data.params.repository_report),
-        branch_report: getReportParam(data.params.branch_report),
-        delivery_agent_report: getReportParam(
-          data.params.delivery_agent_report
-        ),
-        governorate_report: getReportParam(data.params.governorate_report),
-        company_report: getReportParam(data.params.company_report),
-        branch_id: data.params.branch_id || undefined,
-        automatic_update_id: data.params.automatic_update_id || undefined,
+    >(
+      createOrdersDocumentationendpoint,
+      {
+        type: data.type,
+        ordersIDs: data.ordersIDs,
       },
-    });
+      {
+        responseType: 'arraybuffer',
+        params:
+          data.type === 'GENERAL'
+            ? {
+                page: data.params.page,
+                size: data.params.size,
+                search: data.params.search || undefined,
+                sort: data.params.sort || undefined,
+                start_date: data.params.start_date || undefined,
+                end_date: data.params.end_date || undefined,
+                delivery_date: data.params.delivery_date || undefined,
+                governorate: data.params.governorate || undefined,
+                status: data.params.status || undefined,
+                delivery_type: data.params.delivery_type || undefined,
+                delivery_agent_id: data.params.delivery_agent_id || undefined,
+                client_id: data.params.client_id || undefined,
+                store_id: data.params.store_id || undefined,
+                product_id: data.params.product_id || undefined,
+                location_id: data.params.location_id || undefined,
+                receipt_number: data.params.receipt_number || undefined,
+                recipient_name: data.params.recipient_name || undefined,
+                recipient_phone: data.params.recipient_phone || undefined,
+                recipient_address: data.params.recipient_address || undefined,
+                deleted: data.params.deleted,
+                statuses: data.params.statuses?.join(',') || undefined,
+                client_report: getReportParam(data.params.client_report),
+                repository_report: getReportParam(
+                  data.params.repository_report
+                ),
+                branch_report: getReportParam(data.params.branch_report),
+                delivery_agent_report: getReportParam(
+                  data.params.delivery_agent_report
+                ),
+                governorate_report: getReportParam(
+                  data.params.governorate_report
+                ),
+                company_report: getReportParam(data.params.company_report),
+                branch_id: data.params.branch_id || undefined,
+                automatic_update_id:
+                  data.params.automatic_update_id || undefined,
+              }
+            : data.params,
+      }
+    );
 
     const contentType = response.headers['content-type'];
 
