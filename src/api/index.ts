acceea4a6e2995e5fb6779e36bf04055a9e67f24
wagihmohamed <wagihm969@gmail.com>
2024-05-09 02:24:36 +0300

    fix: Handle refresh token expiry error on logout

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */ /* eslint-disable no-underscore-dangle */
 import { refreshTokenService } from '@/services/refreshToken';
-import { signOutService } from '@/services/signOut';
 import { authStore } from '@/store/authStore';
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const baseURL = import.meta.env.VITE_DEV_BASE_URL as string;
@@ -27,74 +25,38 @@ api.interceptors.request.use(
   }
 );
 
-let isRefreshing = false;
-let failedQueue: {
-  resolve: (token: string) => void;
-  reject: (error: AxiosError) => void;
-}[] = [];
-
-const processQueue = (
-  error: AxiosError | null,
-  token: string | null = null
-) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(token!);
-    }
-  });
-  failedQueue = [];
-};
-
 api.interceptors.response.use(
   async (response) => {
     return response;
   },
   async (error) => {
     const originalRequest = error.config;
-    if (
-      error.response &&
-      error.response.status === 401 &&
-      !originalRequest?.retry
-    ) {
-      if (isRefreshing) {
-        // Token refresh is in progress, queue the request
-        return new Promise<string>((resolve, reject) => {
-          failedQueue.push({ resolve, reject });
-        })
-          .then((token) => {
-            originalRequest!.headers.Authorization = `Bearer ${token}`;
-            return axios(originalRequest!);
-          })
-          .catch((err) => {
-            return Promise.reject(err);
-          });
-      }
-      originalRequest!.retry = true;
-      isRefreshing = true;
-      const refreshToken = localStorage.getItem('refreshToken');
-      if (refreshToken) {
-        try {
-          const response = await refreshTokenService(refreshToken);
-          localStorage.setItem('token', response.token);
-          processQueue(null, response.token);
-          return await api(originalRequest!);
-        } catch (e) {
-          processQueue(e as any, null);
-          await signOutService().then(() => {
-            authStore.getState().logout();
-            toast.error('تم انتهاء الجلسة');
-          });
-        } finally {
-          isRefreshing = false;
-        }
-      } else {
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (error.response.status === 401) {
+      const isRefreshTokenRequest =
+        originalRequest?.url?.includes('refresh-token');
+
+      if (isRefreshTokenRequest) {
         authStore.getState().logout();
         toast.error('تم انتهاء الجلسة');
         return Promise.reject(error);
       }
+
+      if (!isRefreshTokenRequest && refreshToken) {
+        try {
+          const response = await refreshTokenService(refreshToken);
+          const newToken = response.token;
+          localStorage.setItem('token', newToken);
+          originalRequest.headers.Authorization = `Bearer ${newToken}`;
+          return await api(originalRequest);
+        } catch (e) {
+          authStore.getState().logout();
+          toast.error('تم انتهاء الجلسة');
+          return Promise.reject(error);
+        }
+      }
     }
+
     return Promise.reject(error);
   }
 );
--- src/services/signOut.ts
+++ src/services/signOut.ts
@@ -3,7 +3,7 @@ import { signOutEndpoint } from '@/api/apisUrl';
 
 export const signOutService = async () => {
   const token = localStorage.getItem('token');
-  const response = await api.post(signOutEndpoint, null, {
+  const response = await api.post(signOutEndpoint, undefined, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
